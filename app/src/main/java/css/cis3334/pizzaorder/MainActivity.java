bf4919379676da2c1a7d75d531858f477cf7f524
Tom Gibbons <tgibbons@css.edu>
2019-02-07 07:10:09 -0600

    Removed code for the Practice Activity

--- app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
+++ app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
@@ -20,23 +20,16 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     TextView txtStatus;
     TextView txtPizzasOrdered;
     Spinner spinnerToppings;
-    PizzaOrderInterface pizzaOrderSystem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // create an pizza order system to use in the class for ordering pizzas.
-        pizzaOrderSystem = new PizzaOrder(this);
         // Set up our radio buttons
         rbSmall = (RadioButton) findViewById(R.id.radioButtonSmall);
         rbMedium = (RadioButton) findViewById(R.id.radioButtonMedium);
         rbLarge = (RadioButton) findViewById(R.id.radioButtonLarge);
-        // review the lines below during the particpation activy and uncomment them
-        rbSmall.append(" -- Price: $" + pizzaOrderSystem.getPrice(Pizza.pizzaSize.SMALL));
-        rbMedium.append(" -- Price: $" + pizzaOrderSystem.getPrice(Pizza.pizzaSize.MEDIUM));
-        rbLarge.append(" -- Price: $" + pizzaOrderSystem.getPrice(Pizza.pizzaSize.LARGE));
 
         // Set up the Check Boxes
         chkbxCheese = (CheckBox) findViewById(R.id.checkBoxCheese);
@@ -58,16 +51,15 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     }
 
     public void onClickOrder(View view) {
-        // ****** Students need to add code here to get information from the UI widgets...
+        // ****** For the Assignment, students need to add code here to get information from the UI widgets...
 
-        // ****** Students need to modify the call to OrderPizza to order the type of pizza the user selects using the UI widgets
-        String orderDescription = pizzaOrderSystem.OrderPizza("Peperoni", "large", false  );
+        String orderDescription = "No orders yet";
 
+        // ****** For the Practice Activity, students need to call to OrderPizza here
+        // ****** For the Assignment, students will modify the order to fit the type of pizza the user selects using the UI widgets
 
         //display a pop up message for a long period of time
         Toast.makeText(getApplicationContext(), "You have ordered a "+orderDescription , Toast.LENGTH_LONG).show();
-        // get the order total from the order system
-        txtTotal.setText("Total Due: " + pizzaOrderSystem.getTotalBill().toString());
         // add this pizza to the textview the lists the pizzas
         txtPizzasOrdered.append(orderDescription+"\n");
 
